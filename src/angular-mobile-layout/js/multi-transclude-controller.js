27e8ed5e55be3c5619aeb770fc9cefa9ae68adef
sgoldin <sgoldin@prepgames.com>
2014-06-11 12:31:24 -0400

    Fix for option header and footer

--- src/angular-mobile-layout/js/multi-transclude-controller.js
+++ src/angular-mobile-layout/js/multi-transclude-controller.js
@@ -3,7 +3,7 @@
 angular.module('mobile.layout')
   .controller('multiTransclude', ['$scope', '$exceptionHandler', function ($scope, $exceptionHandler) {
     var transcludePostLinkers = [];
-    var completedPostLinks = 0;
+    var completedPostLinks = 0, transcludeElements = 0;
 
     var doPostLinking = function () {
       for (var i = 0; i < transcludePostLinkers.length; i++) {
@@ -26,7 +26,7 @@ angular.module('mobile.layout')
       transcludePostLinkComplete: function () {
         completedPostLinks++;
 
-        if (completedPostLinks >= transcludePostLinkers.length * 2) {
+        if (completedPostLinks >= transcludeElements) {
           doPostLinking();
         }
       },
